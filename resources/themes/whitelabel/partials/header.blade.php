c3f509eb8fd63b3a851b5409e9af36d6a525f508
snipe <snipe@snipe.net>
2015-10-29 23:55:30 -0700

    Base whitelabel theme

---
+++ resources/themes/whitelabel/partials/header.blade.php
@@ -0,0 +1,48 @@
+
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="{{ Config::get('app.url') }}">{{ $whitelabel_group->name }}</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+		  @if (Auth::check())
+		  <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="{{ $user->gravatar() }}" class="avatar">{{ $user->name }} <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="{{ route('logout') }}">Logout</a></li>
+            </ul>
+          </li>
+
+		  @else
+		  	<li><a href="{{ route('login') }}">Login</a> </li>
+			<li><a href="{{ route('register') }}">Register</a></li>
+		  @endif
+
+
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<section class="container">
+    <div class="row">
+	<!-- Notifications -->
+	@include('notifications')
+	</div>
+</section>
