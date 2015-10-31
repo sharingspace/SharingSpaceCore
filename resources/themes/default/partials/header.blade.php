b354fa855416e91b371f2d97955402d6e8c38f70
snipe <snipe@snipe.net>
2015-10-31 16:45:57 -0700

    Minor temp changes to main site layout

--- resources/themes/default/partials/header.blade.php
+++ resources/themes/default/partials/header.blade.php
@@ -10,17 +10,16 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="{{ Config::get('app.url') }}">My Awesome Site</a>
+      <a class="navbar-brand" href="{{ Config::get('app.url') }}">AnySha.re</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
+
 
       <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#">About</a></li>
+          <li class="active"><a href="#">Get Started</a></li>
 		  @if (Auth::check())
 		  <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="{{ $user->gravatar() }}" class="avatar">{{ $user->name }} <span class="caret"></span></a>
