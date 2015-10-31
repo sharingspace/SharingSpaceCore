4777242d3cb2aea94ab791fd3d3ae5125c1f82e3
snipe <snipe@snipe.net>
2015-10-31 16:47:02 -0700

    Removed active classes on nav

--- resources/themes/default/partials/header.blade.php
+++ resources/themes/default/partials/header.blade.php
@@ -18,8 +18,8 @@
 
 
       <ul class="nav navbar-nav navbar-right">
-          <li class="active"><a href="#">About</a></li>
-          <li class="active"><a href="#">Get Started</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Get Started</a></li>
 		  @if (Auth::check())
 		  <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="{{ $user->gravatar() }}" class="avatar">{{ $user->name }} <span class="caret"></span></a>
