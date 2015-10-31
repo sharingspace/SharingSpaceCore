bbb4fa4c0cf24a5cd65b01e47338d3383266eb2a
snipe <snipe@snipe.net>
2015-10-31 16:44:53 -0700

    Clearer company HP

--- resources/themes/default/views/home.blade.php
+++ resources/themes/default/views/home.blade.php
@@ -5,7 +5,7 @@
 
 <section class="container">
    <div class="row">
-       <h2>Welcome to AnySha.re</h2>
+       <h2>Welcome to AnySha.re. We allow you to create awesome sharing communities. You should totes sign up and stuff.</h2>
 </div><!--end row-->
 </section>
 @stop
