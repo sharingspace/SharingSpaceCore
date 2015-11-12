855bc9ae7964e69339ecd502bee99fd84d20bd55
snipe <snipe@snipe.net>
2015-11-12 04:25:40 -0800

    Updated strings

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -12,4 +12,19 @@ return [
     'copyright' => 'Copyright',
     'no_about_data'  => 'The owner of this group has not entered any information about it yet.',
 
+    'nav'  => [
+          'browse' => 'Browse',
+          'add' => 'Add',
+          'login' => 'Sign-Up',
+          'logout' => 'Logout',
+          'register' => 'Sign-In',
+    ],
+
+    'entries'  => [
+          'title' => 'Title',
+          'author' => 'Author',
+          'location' => 'Location',
+          'created_at' => 'Created',
+    ],
+
 ];
