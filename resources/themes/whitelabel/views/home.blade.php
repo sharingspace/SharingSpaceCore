6328ee6b7a3871e3eb82b50b8b6673c389815f29
snipe <snipe@snipe.net>
2015-11-12 03:25:25 -0800

    Hook up browse link, use translation strings for no data message

--- resources/themes/whitelabel/views/home.blade.php
+++ resources/themes/whitelabel/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.master')
 
 @section('content')
-<script src="/js/extensions/mobile/bootstrap-table-mobile.js"></script>
 
 <section class="container">
   <div class="row">
@@ -10,11 +9,11 @@
           @if ($whitelabel_group->about!='')
             {{ $whitelabel_group->about }}
           @else
-            The owner of this group has not entered any information about it yet.
+            {{ trans('general.no_about_data') }}
           @endif
         </p>
 
-        <p class="text-center"><a href="" class="btn btn-default">{{ trans('general.browse_button') }}</a></p>
+        <p class="text-center"><a href="{{ route('browse') }}" class="btn btn-default">{{ trans('general.browse_button') }}</a></p>
   </div><!--end row-->
 </section>
 @stop
