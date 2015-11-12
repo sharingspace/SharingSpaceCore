55541b6801b084c2a901c929bfe552868cf73fae
snipe <snipe@snipe.net>
2015-11-12 03:24:45 -0800

    Cleaner indenting, more laravel-esque asset calls

--- resources/themes/whitelabel/layouts/master.blade.php
+++ resources/themes/whitelabel/layouts/master.blade.php
@@ -1,52 +1,50 @@
 <!DOCTYPE html>
 <html lang="en-us">
+  <head>
+    <!-- *** General page information *** -->
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge" /><![endif]-->
+    <meta charset="UTF-8"/>
+    <title>
+      @section('title')
+        {{ $whitelabel_group->name }}
+      @show
+    </title>
+    <!-- Mobile Specific Metas-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Template info -->
+    <meta name="author" content="A. Gianotto">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
 
-<head>
-	<!-- *** General page information *** -->
-		<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge" /><![endif]-->
-		<meta charset="UTF-8"/>
-        <title>
-            @section('title')
-             {{ $whitelabel_group->name }}
-            @show
-        </title>
-		<!-- Mobile Specific Metas-->
-		<metja name="viewport" content="width=device-width, initial-scale=1">
-		<!-- Template info -->
-		<meta name="author" content="A. Gianotto">
-		<meta name="description" content="">
-		<meta name="keywords" content="">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-social.css') }}">
 
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/css/bootstrap-social.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <!-- Bootstrap Table style -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.8.1/bootstrap-table.min.css" type="text/css" media="screen" />
 
-		<!-- Bootstrap Table style -->
-		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.8.1/bootstrap-table.min.css" type="text/css" media="screen" />
+    <!-- jQuery 2.1.3-->
+    <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
 
-		<!-- jQuery 2.1.3-->
-		<script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+    <!-- Bootstrap 3 and Bootstrap Table Javascript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.8.1/bootstrap-table-all.js"></script>
 
-		<!-- Bootstrap 3 and Bootstrap Table Javascript -->
-		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.8.1/bootstrap-table-all.js"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ Theme::asset('css/styles.css', null, true) }}">
 
-		<link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="{{ Theme::asset('css/styles.css', null, true) }}"/>
-
-
-</head>
+  </head>
 
 <body>
 
-	<div>@include('partials.header')</div>
+  <div>@include('partials.header')</div>
 
-	<div>@yield('content')</div>
+  <div>@yield('content')</div>
 
-	<div>@include('partials.footer')</div>
+  <div>@include('partials.footer')</div>
 
 </body>
 </html>
