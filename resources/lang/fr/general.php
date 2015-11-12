3bca07c7c2e95645dd2f46874969434428eb4c08
snipe <snipe@snipe.net>
2015-11-12 04:24:49 -0800

    French strings

---
+++ resources/lang/fr/general.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Site Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'browse_button' => 'Recherchez Ici',
+    'copyright' => 'Droit d\'auteur',
+    'no_about_data'  => 'Le propriétaire de cette communauté n\'est pas entré aucune information à ce sujet encore.',
+
+    'nav'  => [
+          'browse' => 'Parcourir',
+          'add' => 'Ajouter',
+          'login' => 'Connexion',
+          'logout' => 'Déconnexion',
+          'register' => 'Inscrivez-vous',
+    ],
+
+    'entries'  => [
+          'title' => 'Titre',
+          'author' => 'Auteur',
+          'location' => 'Emplacement',
+          'created_at' => 'Crée',
+    ],
+
+
+];
