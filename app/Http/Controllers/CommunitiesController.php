865a687e471e17d3204450c76e01dd528373fc3a
snipe <snipe@snipe.net>
2015-11-19 15:26:37 -0800

    Save community

--- app/Http/Controllers/CommunitiesController.php
+++ app/Http/Controllers/CommunitiesController.php
@@ -8,6 +8,8 @@ use Auth;
 use Theme;
 use App\Entry;
 use Input;
+use Validator;
+use Redirect;
 
 class CommunitiesController extends Controller
 {
@@ -95,5 +97,43 @@ class CommunitiesController extends Controller
     $members = $request->whitelabel_group->members()->get();
     return view('members')->with('members',$members);
   }
+
+  /*
+  Get the create community page
+  */
+  public function getCreate()
+  {
+    return view('communities.edit');
+  }
+
+
+  /*
+  * Save new community
+  */
+  public function postCreate(Request $request)
+  {
+    $community = new \App\Community();
+
+    $validator = Validator::make(Input::all(), $community->rules);
+
+    if ($validator->fails()) {
+      return Redirect::back()->withInput()->withErrors($validator->messages());
+    } else {
+
+      $community->name	= e(Input::get('name'));
+      $community->subdomain	= e(Input::get('subdomain'));
+
+      if ($community->save()) {
+        return redirect('/')->with('success','Success!');
+
+      } else {
+        echo 'wut.';
+      }
+
+    }
+
+  }
+
+
 
 }
