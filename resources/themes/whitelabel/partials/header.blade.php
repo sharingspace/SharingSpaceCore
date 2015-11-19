7ae0bf37d0c7ecc2c43fdf63865f71d7d9e294e0
snipe <snipe@snipe.net>
2015-11-19 09:51:12 -0800

    Added link to profile

--- resources/themes/whitelabel/partials/header.blade.php
+++ resources/themes/whitelabel/partials/header.blade.php
@@ -11,6 +11,7 @@
         <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="{{ Auth::user()->gravatar() }}" class="avatar-sm">{{ Auth::user()->getDisplayName() }} <span class="caret"></span></a>
               <ul class="dropdown-menu">
+                <li><a href="{{ route('user.profile') }}"><i class="fa fa-user"></i> {{ trans('general.nav.profile') }}</a></li>
                 <li><a href="{{ route('user.settings') }}"><i class="fa fa-gears"></i> {{ trans('general.nav.settings') }}</a></li>
                 <li><a href="{{ route('logout') }}"><i class="fa fa-power-off"></i> {{ trans('general.nav.logout') }} </a></li>
               </ul>
