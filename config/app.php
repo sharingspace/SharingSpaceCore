c337ebd430cd72a0a625a5012a3a302ee942fab5
snipe <snipe@snipe.net>
2015-11-19 15:26:04 -0800

    Added Socialite Facade

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Chrisbjr\ApiGuard\Providers\ApiGuardServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
         Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class,
+        Laravel\Socialite\SocialiteServiceProvider::class,
 
     ],
 
@@ -209,6 +210,7 @@ return [
         'Route'     => Illuminate\Support\Facades\Route::class,
         'Schema'    => Illuminate\Support\Facades\Schema::class,
         'Session'   => Illuminate\Support\Facades\Session::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'Storage'   => Illuminate\Support\Facades\Storage::class,
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
