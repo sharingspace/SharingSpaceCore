c0aaa27ca5e382959c391ca759992812f0302321
snipe <snipe@snipe.net>
2015-11-18 22:28:59 -0800

    Members basic layout

--- resources/themes/whitelabel/views/members.blade.php
+++ resources/themes/whitelabel/views/members.blade.php
@@ -11,9 +11,17 @@
 
 <section class="container">
 <div class="row">
-  
+
   @foreach ($members as $member)
-    <li>{{ $member->email }}
+    <div class="col-md-4">
+      <div class="col-md-4">
+        <img src="{{ $member->gravatar() }}">
+      </div>
+      <div class="col-md-8">
+        <h4>{{ $member->getDisplayName() }}</h4>
+        <p>{{ $member->bio }}</p>
+      </div>
+    </div>
   @endforeach
 
   </div>
