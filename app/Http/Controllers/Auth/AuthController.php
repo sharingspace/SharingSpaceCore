f9f3c16269bd3f4e90a5fe657f5192083ccefa06
snipe <snipe@snipe.net>
2015-11-19 16:20:00 -0800

    Fixed field name

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -98,7 +98,7 @@ class AuthController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'displayname' => $data['displayname'],
+            'display_name' => $data['display_name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
