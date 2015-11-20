2fd6237fbcb04d63a404a294edceb350179e31ed
snipe <snipe@snipe.net>
2015-11-19 16:19:31 -0800

    Generic subnav so as not to confuse anyone

--- resources/themes/whitelabel/views/account/settings.blade.php
+++ resources/themes/whitelabel/views/account/settings.blade.php
@@ -230,42 +230,63 @@
 
 						<!-- SIDE NAV -->
 						<ul class="side-nav list-group margin-bottom-60" id="sidebar-nav">
-							<li class="list-group-item"><a href="#"><i class="fa fa-eye"></i> PROFILE</a></li>
-							<li class="list-group-item"><a href="#"><i class="fa fa-tasks"></i> PROJECTS</a></li>
-							<li class="list-group-item"><a href="#"><i class="fa fa-comments-o"></i> COMMENTS</a></li>
-							<li class="list-group-item"><a href="#"><i class="fa fa-history"></i> HISTORY</a></li>
-							<li class="list-group-item active"><a href="#"><i class="fa fa-gears"></i> SETTINGS</a></li>
+							<li class="list-group-item"><a href="#"><i class="fa fa-eye"></i> SUBNAV</a></li>
+							<li class="list-group-item"><a href="#"><i class="fa fa-comments-o"></i> SUBNAV</a></li>
+							<li class="list-group-item"><a href="#"><i class="fa fa-history"></i> SUBNAV</a></li>
+							<li class="list-group-item active"><a href="#"><i class="fa fa-gears"></i> SUBNAV</a></li>
 						</ul>
 						<!-- /SIDE NAV -->
 
 
-						<!-- info -->
-						<div class="box-light margin-bottom-30"><!-- .box-light OR .box-light -->
+            <!-- info -->
+						<div class="box-light margin-bottom-30">
+
 							<div class="row margin-bottom-20">
 								<div class="col-md-4 col-sm-4 col-xs-4 text-center bold">
 									<h2 class="size-30 margin-top-10 margin-bottom-0 font-raleway">12</h2>
-									<h3 class="size-11 margin-top-0 margin-bottom-10 text-info">PROJECTS</h3>
+									<h3 class="size-11 margin-top-0 margin-bottom-10 text-info">ENTRIES</h3>
 								</div>
 
 								<div class="col-md-4 col-sm-4 col-xs-4 text-center bold">
 									<h2 class="size-30 margin-top-10 margin-bottom-0 font-raleway">34</h2>
-									<h3 class="size-11 margin-top-0 margin-bottom-10 text-info">TASKS</h3>
+									<h3 class="size-11 margin-top-0 margin-bottom-10 text-info">FOLLOWING</h3>
 								</div>
 
 								<div class="col-md-4 col-sm-4 col-xs-4 text-center bold">
 									<h2 class="size-30 margin-top-10 margin-bottom-0 font-raleway">32</h2>
-									<h3 class="size-11 margin-top-0 margin-bottom-10 text-info">UPLOADS</h3>
+									<h3 class="size-11 margin-top-0 margin-bottom-10 text-info">COMMUNITIES</h3>
 								</div>
+
 							</div>
 							<!-- /info -->
 
-							<div class="text-muted">
-								<ul class="list-unstyled nomargin">
-									<li class="margin-bottom-10"><i class="fa fa-globe width-20 hidden-xs hidden-sm"></i> <a href="http://www.stepofweb.com">www.stepofweb.com</a></li>
-									<li class="margin-bottom-10"><i class="fa fa-facebook width-20 hidden-xs hidden-sm"></i> <a href="http://www.facebook.com/stepofweb">stepofweb</a></li>
-									<li class="margin-bottom-10"><i class="fa fa-twitter width-20 hidden-xs hidden-sm"></i> <a href="http://www.twitter.com/stepofweb">@stepofweb</a></li>
-								</ul>
-							</div>
+              <!-- social / stats -->
+              <div class="text-muted">
+
+                <ul class="list-unstyled nomargin">
+                  @if (Auth::user()->website)
+                    <li class="margin-bottom-10"><i class="fa fa-globe width-20 hidden-xs hidden-sm"></i> <a href="{{ Auth::user()->website }}">{{ Auth::user()->website }}</a></li>
+                  @endif
+
+                  @if (Auth::user()->twitter)
+                    <li class="margin-bottom-10"><i class="fa fa-twitter width-20 hidden-xs hidden-sm"></i> <a href="{{ Auth::user()->twitter }}">Twitter</a></li>
+                  @endif
+
+                  @if (Auth::user()->facebook)
+                    <li class="margin-bottom-10"><i class="fa fa-facebook width-20 hidden-xs hidden-sm"></i> <a href="{{ Auth::user()->facebook }}">Facebook</a></li>
+                  @endif
+
+                  @if (Auth::user()->pinterest)
+                    <li class="margin-bottom-10"><i class="fa fa-pinterest width-20 hidden-xs hidden-sm"></i> <a href="{{ Auth::user()->pinterest }}">Pinterest</a></li>
+                  @endif
+
+                  @if (Auth::user()->google)
+                    <li class="margin-bottom-10"><i class="fa fa-google width-20 hidden-xs hidden-sm"></i> <a href="{{ Auth::user()->google }}">Google +</a></li>
+                  @endif
+
+                </ul>
+              </div>
+              <!-- /social / stats -->
 
 						</div>
 
