c6bae0a40b7a706b29dd2d89c5471a2568a049c0
snipe <snipe@snipe.net>
2016-01-07 02:08:00 -0800

    Simplified layout for entry form

--- resources/themes/whitelabel/views/entries/edit.blade.php
+++ resources/themes/whitelabel/views/entries/edit.blade.php
@@ -12,61 +12,46 @@
 
 <!-- -->
 			<section>
+
 				<div class="container margin-top-20">
 
 					<div class="row">
 
-						<!-- LEFT TEXT -->
-						<div class="col-md-5">
-
-							<h2 class="size-16">CREATE A NEW ENTRY</h2>
-							<p class="text-muted">Maecenas metus nulla, commodo a sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa.</p>
-							<p class="text-muted">Sodales sed, dignissim pretium nunc. Nam et lacus neque. Ut enim massa, sodales tempor convallis et, iaculis ac massa.</p>
-
-						</div>
-						<!-- /LEFT TEXT -->
-
 
 						<!-- Entry -->
-						<div class="col-md-7">
-              <div class="col-md-12">
-							  <h2 class="size-16">NEW ENTRY</h2>
-              </div>
-
-							<!-- login form -->
-							<form method="post" action="{{ route('entry.create.save') }}" enctype="multipart/form-data" autocomplete="off" class="nomargin">
-                {!! csrf_field() !!}
-
-								<div class="clearfix">
-
-                  <fieldset class="nomargin">
-
-                    <div class="col-md-3 form-group margin-bottom-10{{ $errors->first('post_type', ' has-error') }}">
-                      <label class="input">
-                        <select class="form-control" name="post_type" id="post_type">
-                          <option value="want">I want</option>
-                          <option value="have">I have</option>
-      									</select>
-                      </label>
-  									</div>
+						<div class="col-md-12">
 
 
-                    <div class="col-md-8 form-group margin-bottom-10{{ $errors->first('title', ' has-error') }}">
-                      <label class="input">
-  										  <input type="text" name="title" class="form-control" placeholder="Description" required="" value="{{ old('subdomain') }}">
-                      </label>
-                      {!! $errors->first('title', '<span class="help-block">:message</span>') !!}
-  									</div>
+                <h2 class="size-16 uppercase">{{ trans('general.entries.create') }}</h2>
+  							<!-- entry form -->
+  							<form method="post" action="{{ route('entry.create.save') }}" enctype="multipart/form-data" autocomplete="off" class="nomargin">
+                  {!! csrf_field() !!}
 
-                    <div class="col-md-1">
-                      <a href="#" class="btn btn-default"><i class="fa fa-ellipsis-h"></i></a>
-                    </div>
-  									<div class="col-md-12 col-sm-12 col-xs-12 text-right">
-  										<button class="btn btn-primary">Create Entry</button>
-  									</div>
+  								<div class="clearfix">
 
-                </fieldset>
-              </div>
+                    <fieldset class="nomargin">
+
+                      <div class="col-md-3 margin-bottom-10{{ $errors->first('post_type', ' has-error') }}">
+        									<select class="form-control" name="post_type" id="post_type">
+                            <option value="want">I want</option>
+                            <option value="have">I have</option>
+        									</select>
+                      </div>
+                      <div class="col-md-8 margin-bottom-10{{ $errors->first('title', ' has-error') }}">
+                        <!-- Name -->
+                        <label class="input">
+        									<input type="text" name="title" class="form-control" placeholder="Description">
+                        </label>
+                      </div>
+                      <div class="col-md-1">
+                        <a href="#" class="btn btn-default"><i class="fa fa-ellipsis-h"></i></a>
+                      </div>
+    									<div class="col-md-12 col-sm-12 col-xs-12 text-right">
+    										<button class="btn btn-primary">Create Entry</button>
+    									</div>
+
+                  </fieldset>
+
 
 							</form>
 						</div>
