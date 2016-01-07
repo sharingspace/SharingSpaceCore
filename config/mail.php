a3ef89dfbf86a7f325ccd1dcad0e0f9b7c603ecd
snipe <snipe@snipe.net>
2016-01-07 07:38:29 -0800

    Added mail defaults

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'snipe@snipe.net', 'name' => 'AnySha.re'],
 
     /*
     |--------------------------------------------------------------------------
