07494ffaa6084f71f2219b0b0ace5ac4fa559281
snipe <snipe@snipe.net>
2016-01-06 17:24:19 -0800

    Pagetheme model
    This doesn't do much yet

---
+++ app/Models/Pagetheme.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Pagetheme extends Model
+{
+
+  /**
+  * The database table used by the model.
+  *
+  * @var string
+  */
+  protected $table = 'themes';
+
+
+
+}
