d9f7e0009084b7c8566fd74b592bb9c9abd17cde
snipe <snipe@snipe.net>
2016-01-07 20:41:11 -0800

    Order history page

---
+++ resources/themes/default/views/account/history.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts/master')
+
+{{-- Page title --}}
+@section('title')
+     {{ trans('general.nav.order_history') }} ::
+@parent
+@stop
+
+
+{{-- Page content --}}
+@section('content')
+
+
+<!-- -->
+			<section>
+				<div class="container margin-top-20">
+					<div class="row">
+
+
+						<div class="col-md-11 col-md-offset-1">
+              <h2 class="size-16 uppercase">{{ trans('general.nav.order_history') }}</h2>
+
+              @foreach ($subscriptions as $subscription)
+                <li>{{ $subscription->community }}
+              @endforeach
+            </div>
+
+					</div>
+				</div>
+			</section>
+			<!-- / -->
+
+
+@stop
