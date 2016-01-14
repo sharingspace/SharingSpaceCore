2bbf70825e348c1c9a07a1c5ee4a59fcf04a4bd2
snipe <snipe@snipe.net>
2016-01-14 15:23:49 -0800

    Added rough edit button

--- resources/themes/whitelabel/views/browse.blade.php
+++ resources/themes/whitelabel/views/browse.blade.php
@@ -19,6 +19,7 @@
               <th data-sortable="true" data-field="author">{{ trans('general.entries.author') }}</th>
               <th data-sortable="true" data-field="location">{{ trans('general.entries.location') }}</th>
               <th data-sortable="true" data-field="created_at">{{ trans('general.entries.created_at') }}</th>
+              <th data-sortable="false" data-field="actions"></th>
           </tr>
       </thead>
     </table>
