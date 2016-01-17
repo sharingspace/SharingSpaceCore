6d1d8ef6c51b8f4ad69bccfbf723f98e364358d2
snipe <snipe@snipe.net>
2016-01-16 18:49:24 -0800

    Added geolocation helper method

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -24,4 +24,26 @@
         throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
       }
 
+      public static function  latlong($location) {
+       		//api call and JSON here
+
+       		if ($location!='') {
+       			try {
+    				$json = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($location));
+    				$parsedjson = json_decode($json, true);
+
+    				if (key_exists(0,$parsedjson['results'])) {
+    					$lat_long_array = $parsedjson['results'][0]['geometry']['location'];
+    					return $lat_long_array;
+    				} else {
+    					return false;
+    				}
+    			} catch (Exception $e) {
+        			//echo 'Caught exception: ',  $e->getMessage(), "\n";
+        			return false;
+    			}
+    		}
+    	}
+
+
   }
