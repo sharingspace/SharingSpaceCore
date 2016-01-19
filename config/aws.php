7f83d5434bdf6becabbabf9d9b856a7e96ca9470
snipe <snipe@snipe.net>
2016-01-18 18:48:49 -0800

    AWS config

---
+++ config/aws.php
@@ -0,0 +1,32 @@
+<?php
+
+use Aws\Laravel\AwsServiceProvider;
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | AWS SDK Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The configuration options set in this file will be passed directly to the
+    | `Aws\Sdk` object, from which all client objects are created. The minimum
+    | required options are declared here, but the full set of possible options
+    | are documented at:
+    | http://docs.aws.amazon.com/aws-sdk-php/v3/guide/guide/configuration.html
+    |
+    */
+
+    'credentials' => [
+        'key'    => env('AWS_ACCESS_KEY_ID', 'us-east-1'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY', ''),
+    ],
+    
+    'region' => 'us-west-2',
+    'version' => 'latest',
+
+    // You can override settings for specific services
+    'Ses' => [
+        'region' => 'us-east-1',
+    ],
+];
