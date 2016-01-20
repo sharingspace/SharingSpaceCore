344ecfb8bad3485b8dc21346a9ff4cb614b06bd2
snipe <snipe@snipe.net>
2016-01-19 20:13:19 -0800

    Added user and upload_key to media table

---
+++ database/migrations/2016_01_19_195407_add_user_to_uploads.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserToUploads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+     public function up()
+     {
+       Schema::table('entries_media', function ($table) {
+         $table->integer('user_id')->nullable()->default(NULL);
+         $table->text('upload_key')->nullable()->default(NULL);
+       });
+       Schema::rename('entries_media', 'media');
+     }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::rename('media', 'entries_media');
+      Schema::table('entries_media', function ($table) {
+         $table->dropColumn('user_id','upload_key');
+      });
+
+    }
+}
