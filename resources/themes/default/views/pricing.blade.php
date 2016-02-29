bc8d605ea5fd3c5cbee33f9494502a76e4ac0de2
David Linnard <dslinnard@gmail.com>
2016-02-28 16:04:57 -0800

    Fixes #113 some more pricing page tweaks

--- resources/themes/default/views/pricing.blade.php
+++ resources/themes/default/views/pricing.blade.php
@@ -12,7 +12,7 @@ table caption {
 
 
 <section id='slider' style="width:100%;">					
-  <div class='page_banner coop_banner'>
+  <div class='page_banner sharing_fixed_banner'>
     <h1>{{trans('pricing.headline') }}</h1>
   </div>
 </section>
@@ -20,28 +20,29 @@ table caption {
   <section id="features">
     <div class="container">
       <div class="table-responsive">
-        <table class="table margin-top-30 margin-bottom-0" style="color:#222;font-size:18px;font-weight:500;">
+        <table class="table margin-top-30 margin-bottom-0" style="color:#222;font-size:16px;font-weight:500;">
         <caption class='sr-only'>Pricing table</caption>
           <thead>
             <tr style="background-color:#ddd;"><th style="width:60%;">Feature</th>
             <th>$10 a month or $100 a year <i class="fa fa-asterisk"></th>
           </thead>
           <tbody>
-            <tr><td>Unlimited Users</td><td><i class='fa fa-check fa-check'></i></td></tr>
-            <tr><td>Unlimited Entries</td><td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>Unlimited users</td><td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>Unlimited entries</td><td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>Your logo</td><td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>API</td><td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>Google Analytics<td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>Open or closed community<td><i class='fa fa-check fa-check'></i></td></tr>
-            <tr><td>SSL Security<td><i class='fa fa-check fa-check'></i></td></tr>
-            <tr><td>Email Support</td><td><i class='fa fa-check fa-check'></i></td></tr>
-            <tr><td>Automatic Backups</td><td><i class='fa fa-check fa-check'></i></td></tr>
-            <tr><td>Automatic Upgrades</td><td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>SSL security<td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>Email support</td><td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>Automatic backups</td><td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>Automatic upgrades</td><td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>Social media sign up/login</td><td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>No setup fees, cancel anytime</td><td><i class='fa fa-check fa-check'></i></td></tr>            
             <tr><td></td><td><a href="{{ route('community.create.form') }}"><button type="button" class="btn btn-sm btn-warning">{{ trans('pricing.startTrial') }}</button></a></td></tr>        
           </tbody>
         </table>
-        <p class="small">No setup fees. Cancel anytime.<br>
-        <i class="fa fa-asterisk"></i> All fees in US dollars</p>
+        <p class="small"><i class="fa fa-asterisk"></i> All fees in US dollars</p>
       </div> 
     </div>
   </section>
