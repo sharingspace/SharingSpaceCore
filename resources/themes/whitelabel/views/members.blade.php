f04817ae35038ce63255c4ecdf97fb7aa3f2b3cd
David Linnard <dslinnard@gmail.com>
2016-02-29 22:03:58 -0800

    Fixes #111 fix language on create page and more
    Tidied up members page and cleaned up indenetation on create page and some wrongly nested bootstrap stuff

--- resources/themes/whitelabel/views/members.blade.php
+++ resources/themes/whitelabel/views/members.blade.php
@@ -6,71 +6,42 @@
 @parent
 @stop
 
-<style>
-.thumbnail {
-    height: 40px;
-    border:none;
-}
 
-.image {
-    width: 100%;
-    height: 100%;
-}
-
-.member_image img {
-    -webkit-transition: all 1s ease; /* Safari and Chrome */
-    -moz-transition: all 1s ease; /* Firefox */
-    -ms-transition: all 1s ease; /* IE 9 */
-    -o-transition: all 1s ease; /* Opera */
-    transition: all 1s ease;
-}
-
-.member_image:hover img {
-		-ms-transform: translate(50px,0) scale(4); 			/* IE 9 */
-    -webkit-transform: translate(50px,0) scale(4); 	/* Safari */
-    transform: translate(50px,0) scale(4); 					/* Standard syntax */
-    o-transform:translate(50px,0) scale(4);
-}
-
-
-table#members thead .sortable {
-  background-position-x: 90%;
-  background-position-y: 50%;
-}
-</style>
 @section('content')
 
 
 <div class="container">
 	<div class="row">
 		<div class="col-lg-10 col-md-12 col-lg-offset-1  col-sm-10 col-sm-offset-1 col-xs-12 margin-top-20">
- 			<table class="table table-condensed" id="members">
-      	<thead>
-          <tr>
-            <th data-sortable="false"><span class='sr-only'>Image</span></th>
-            <th data-sortable="true">Name</th>
-            <th data-sortable="true">Location</th>
-            <th data-sortable="false">Bio</th>
-            <th data-sortable="true">Since</th>
-          </tr>
-        </thead>
-				<tbody>
-					@foreach ($members as $member)
-						<tr>
-              <td class="thumbnail" style="border:none;"><div class="member_image"><img src="{{ $member->gravatar() }}" class="thumbnail pull-left"></div></td>
-              <td>{{ $member->getDisplayName() }}
+      <div class="table-responsive">
+   			<table class="table table-condensed" id="members">
+        	<thead>
+            <tr>
+              <th data-sortable="false"><span class='sr-only'>Image</span></th>
+              <th data-sortable="true">Name</th>
+              <th data-sortable="true">Location</th>
+              <th data-sortable="false" class="bio">Bio</th>
+              <th data-sortable="true">Since</th>
+            </tr>
+          </thead>
+  				<tbody>
+  					@foreach ($members as $member)
+  						<tr>
+                <td class="member_thumb"><img src="{{ $member->gravatar() }}"></div></td>
+                <td><a href="{{ route('user.profile', [$member->id]) }}">{{ $member->getDisplayName() }}</a>
 
-              @if ($member->pivot->is_admin==1)
-                <i class="fa fa-star text-warning"></i>
-              @endif
-              </td>
-              <td>{{$member->location}}</td>
-              <td>{{ substr_replace($member->bio, '&hellip;', 60) }} <a href="{{ route('user.profile', [$member->id]) }}">more</a></td>
-              <td>{{date("m-Y", strtotime($member->created_at))}}</td>
-						</tr>
-					@endforeach
-    		</tbody>
-    	</table>
+                @if ($member->pivot->is_admin==1)
+                  <i class="fa fa-star text-warning"></i>
+                @endif
+                </td>
+                <td>{{$member->location}}</td>
+                <td>{{ substr_replace($member->bio, '&hellip;', 200) }}</td>
+                <td>{{date("m-Y", strtotime($member->created_at))}}</td>
+  						</tr>
+  					@endforeach
+      		</tbody>
+      	</table>
+      </div>
  	 </div> <!-- col-lg-10 -->
 	</div> <!-- row -->
 </div> <!-- #container -->
