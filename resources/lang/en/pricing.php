784467984162f6ed167a188537d1dce89b03a3ae
David Linnard <dslinnard@gmail.com>
2016-03-17 21:54:31 -0700

    Fixed assistance route name
    also fixed some language errors on pricing table and  made table font smaller on mobile so table would fit

--- resources/lang/en/pricing.php
+++ resources/lang/en/pricing.php
@@ -9,11 +9,14 @@ return [
     */
 
     'headline' => 'Pricing',
+    'cost' => '$10 a month or $100 a year',
+    'feature' => 'Feature',
     'startTrial' => 'Start your free trial',
     'unlimited_users' => 'Unlimited users',
     'unlimited_entries' => 'Unlimited entries',
     'your_logo' => 'Your logo',
     'api' => 'API',
+    'ssl' => 'SSL',
     'analytics' => 'Google Analytics',
     'email_support' => 'Email support',
     'backups' => 'Automatic backups',
@@ -21,6 +24,13 @@ return [
     'no_setup_fees' => 'No setup fees, cancel anytime',
     'fees_us_dollars' => 'All fees in US dollars',
     'for_free' => 'Get AnyShare for Free',
+    'apply_now' => 'Apply Now',
+    'q_a' => [
+        'q1' => 'Q. I can\'t afford AnyShare right now. Can I still use it?',
+        'a1' => 'A. We don’t want to stop groups that don’t have the budget to pay, from using AnyShare. If this applies to you, you can apply to receive AnyShare for free.',
+        'q2' => 'Q. What if I want to cancel my subscription?',
+        'a2' => 'A. You can cancel your subscription at any time. Before doing so, you can keep your data, by using the inbuilt api.',
+    ],
     'financial_assist' => [
       'success' => 'You application for financial assistance has been successfully sent',
       'error' => 'We seem to be having a problem processing your application',
--- resources/themes/default/views/pricing.blade.php
+++ resources/themes/default/views/pricing.blade.php
@@ -11,18 +11,18 @@
   <section id="pricing">
     <div class="container">
       <div class="table-responsive">
-        <table class="table margin-top-30 margin-bottom-0" style="color:#222;font-size:16px;font-weight:500;">
+        <table class="table margin-top-30 margin-bottom-0">
         <caption class='sr-only'>{{ trans('pricing.headline') }}</caption>
           <thead>
-            <tr style="background-color:#ddd;"><th style="width:60%;">Feature</th>
-            <th>$10 a month or $100 a year <i class="fa fa-asterisk"></th>
+            <tr style="background-color:#ddd;"><th style="width:60%;">{{ trans('pricing.feature') }}</th>
+            <th>{{ trans('pricing.cost') }} <i class="fa fa-asterisk"></th>
           </thead>
           <tbody>
             <tr><td>{{ trans('pricing.unlimited_users') }}</td><td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>{{ trans('pricing.unlimited_entries') }}</td><td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>{{ trans('pricing.your_logo') }} Your logo</td><td><i class='fa fa-check fa-check'></i></td></tr>
-            <tr><td>{{ trans('pricing.API') }}</td><td><i class='fa fa-check fa-check'></i></td></tr>
-            <tr><td>{{ trans('pricing.analytics') }} Google Analytics<td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>{{ trans('pricing.api') }}</td><td><i class='fa fa-check fa-check'></i></td></tr>
+            <tr><td>{{ trans('pricing.analytics') }}<td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>{{ trans('pricing.ssl') }}<td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>{{ trans('pricing.email_support') }}</td><td><i class='fa fa-check fa-check'></i></td></tr>
             <tr><td>{{ trans('pricing.backups') }}</td><td><i class='fa fa-check fa-check'></i></td></tr>
@@ -35,10 +35,10 @@
       </div> 
 
       <dl>
-        <dt>Q. I can’t afford AnyShare right now. Can I still use it?</dt>
-        <dd>A. We don’t want to stop groups that don’t have the budget to pay, from using AnyShare.<br>If this applies to you, you can apply to receive AnyShare for free. <strong><a href="/financial_assist">Apply Now</a></strong></dd>
-        <dt>Q. What if I want to cancel my subscription?</dt>
-        <dd>You can cancel your subscription at any time. Before doing so, you can keep your data, by using the inbuilt api.</dd>
+        <dt>{{ trans('pricing.q_a.q1') }}</dt>
+        <dd>{{ trans('pricing.q_a.a1') }} <strong><a href="{{route('assistance')}}">{{ trans('pricing.apply_now') }}</a></strong></dd>
+        <dt>{{ trans('pricing.q_a.q2') }}</dt>
+        <dd>{{ trans('pricing.q_a.a2') }}</dd>
       </dl>
 
     </div>
