ca17bc78b9e6c05d0f837d7e3c8e17d9d782b056
snipe <snipe@snipe.net>
2016-03-24 14:31:24 -0700

    Remove basic auth, since we don't use it

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,6 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'subdomain' => \App\Http\Middleware\SubdomainMiddleware::class,
         'community-auth'=>\App\Http\Middleware\CommunityPermissionMiddleware::class,
