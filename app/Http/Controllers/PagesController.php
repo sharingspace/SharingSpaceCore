59d57f003a619a34fa6b2cd3d703a4bd19df7c61
snipe <snipe@snipe.net>
2016-03-24 16:38:38 -0700

    Exra checks to see if the login page should be branded or not

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Auth;
 use Theme;
+use Log;
 
 class PagesController extends Controller
 {
@@ -14,15 +15,27 @@ class PagesController extends Controller
   {
 
     if ($request->whitelabel_group) {
+        LOG::debug('Whitelabel routing: Passed middleware, start getHomepage');
         if (Auth::check()) {
             if (Auth::user()->canSeeCommunity($request->whitelabel_group)) {
+                LOG::debug('Whitelabel routing: User can see this homepage');
                 $entries = $request->whitelabel_group->entries()->with('author','exchangeTypes','media')->orderBy('created_at','desc')->get();
                 return view('home')->with('entries',$entries);
             } else {
-                return view('request-access');
+                LOG::debug('Whitelabel routing: User is logged in but cannot see this homepage');
+                return view('request-access')->withError('You must be a member of this group to view this page.');
+            }
+
+
+        } else {
+            if ($request->whitelabel_group->group_type == 'S') {
+                return view('auth/login-unbranded')->withError('You must be logged in and a member to see this.');
+            } else {
+                return view('auth/login')->withError('You must be logged in and a member to see this.');
             }
 
         }
+
 
 
     } else {
