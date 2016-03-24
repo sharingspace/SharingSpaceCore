f64324a5959cd7327b5c1cb39ceffa7513305c57
snipe <snipe@snipe.net>
2016-03-24 14:32:02 -0700

    Extra logging to debug

--- app/Http/Middleware/CommunityPermissionMiddleware.php
+++ app/Http/Middleware/CommunityPermissionMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Auth;
+use Log;
 
 
 class CommunityPermissionMiddleware
@@ -20,9 +21,10 @@ class CommunityPermissionMiddleware
 
         // This is a whitelabel group, otherwise skip it
         if ($request->whitelabel_group) {
-
+            LOG::debug('Whitelabel routing: This is a valid whitelabel group.');
           // This group is not restricted
           if ($request->whitelabel_group->group_type=='O') {
+              LOG::debug('Whitelabel routing: This is an open group');
               return $next($request);
 
           } else {
@@ -30,17 +32,24 @@ class CommunityPermissionMiddleware
             // and are allowed to see this group
             if (Auth::check()) {
 
+                LOG::debug('Whitelabel routing: User is logged in.');
                 if (Auth::user()->canSeeCommunity($request->whitelabel_group)) {
+                    LOG::debug('Whitelabel routing: This user is authorized to see this.');
                     return $next($request);
                 } else {
-                    return redirect(route('login'))->withError('You must be a member of this group to view this page.');
+                    LOG::debug('Whitelabel routing: User is not allowed to view this group.');
+                    return redirect(route('community.request-access.form'))->withError('You must be a member of this group to view this page.');
                 }
 
             // User is not logged in
             } else {
+                LOG::debug('Whitelabel routing: User is not logged in.');
                 if ($request->whitelabel_group->group_type=='S') {
+                    LOG::debug('Whitelabel routing: User is not logged in and this community requires authorization.');
                     return redirect(route('login'))->withError('You must be logged in and a member to see this.');
                 }
+                LOG::debug('Whitelabel routing: User is not logged in, but the community does not require authorization.');
+                return $next($request);
 
             }
 
