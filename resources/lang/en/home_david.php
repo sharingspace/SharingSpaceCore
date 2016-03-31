5b28f4eba6cb3a5b80266805363b5e42a703c021
David Linnard <dslinnard@gmail.com>
2016-03-30 20:00:43 -0700

    Temporary HP copies
    https://massmosaic.app/three

---
+++ resources/lang/en/home_david.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Site Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+
+         /* David */
+        
+            'homepage' => 'home page',
+    'home_headline' => 'Be a Sharing Community',
+    'subhome_headline' => 'We Make it Easy',
+    'subhome_headline2' => 'We Make it Easy for You',
+
+    'our_communities'  => 'Our Communities',
+        'learn_more_headline' => 'Learn about "Sharing Hubs"',
+        'learn_more_subheadline'=> 'Learn about "Sharing Hubs"',
+        'learn_more_p1a' => 'AnyShare provides you the building blocks to build your own Sharing website, with no technical experience required.',
+
+        'learn_more_p1b' => '',
+        'learn_more_p2' => 'Any business, group or community can create their own Sharing Hub and you get to choose how your users exchange; rent, trade, gift, borrow, buy/sell or any combination.',
+        'learn_more_p2' => 'Any community, group or business can create their own',
+        'learn_more_p2b' => ' and you get to choose how users exchange - rent, trade, gift, borrow, buy, sell or any combination.',
+        'learn_more_p3a' => 'Free trial your own',
+         'learn_more_p3b' => 'Sharing Hub',
+         'learn_more_p3c' =>  'today and join the Sharing Community.',  
+        
+];
