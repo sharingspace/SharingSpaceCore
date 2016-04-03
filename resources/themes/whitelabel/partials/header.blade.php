bd3a6a21a5dd33516ff9b5a75885e90e04379c76
David Linnard <dslinnard@gmail.com>
2016-04-02 19:34:29 -0700

    Fixes can update community

--- resources/themes/whitelabel/partials/header.blade.php
+++ resources/themes/whitelabel/partials/header.blade.php
@@ -132,7 +132,7 @@
                       </button>
                     </a>
                   </li>
-                  @can ('update-community')
+                  @can('update-community', $whitelabel_group)
                   <li>
                     <a href="{{ route('community.edit.form')}}"><i class="fa fa-cogs"></i></a>
                   </li>
