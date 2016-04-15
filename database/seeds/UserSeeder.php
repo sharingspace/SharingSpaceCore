dc8a22315b27ff7edc9d1ea1021709d050e0f012
snipe <snipe@snipe.net>
2016-04-14 17:52:38 -0700

    Attach entries and communities to the user
    This may result in orphaned entries - need to follow up

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -7,6 +7,10 @@ class UserSeeder extends Seeder
 {
   public function run()
   {
-      factory(User::class, 'user',25)->create();
+    factory(User::class, 'user', 5)->create()->each(function($u) {
+        $u->entries()->save(factory(App\Entry::class,'entry')->make());
+        $u->communities()->save(factory(App\Community::class, 'community')->make());
+    });
   }
+
 }
