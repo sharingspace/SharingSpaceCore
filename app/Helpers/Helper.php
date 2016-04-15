ead93a4e86f3ec6f439980eef5f76ef39805c72c
snipe <snipe@snipe.net>
2016-04-14 18:25:06 -0700

    Fixed indenting

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,9 +1,24 @@
 <?php
-
+/**
+ * This contains seom static helpers for
+ * the AnyShare application.
+ *
+ * PHP version 5.5.9
+ *
+ * @package AnyShare
+ * @version v1.0
+ */
 
 class Helper
 {
 
+    /**
+    * Returns a CDN url
+    *
+    * @author [A. Gianotto] [<snipe@snipe.net>]
+    * @since  [v1.0]
+    * @return String
+    */
     public static function cdn_assets($file)
     {
         static $manifest = null;
@@ -24,6 +39,15 @@ class Helper
         throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
     }
 
+
+
+    /**
+    * Returns the latitude and longitude of an address
+    *
+    * @author [A. Gianotto] [<snipe@snipe.net>]
+    * @since  [v1.0]
+    * @return Array
+    */
     public static function latlong($location)
     {
           //api call and JSON here
@@ -48,49 +72,89 @@ class Helper
 
 
 
-    public static function relativeTime($ts) {
+    /**
+    * Returns relative time, like "one day ago"
+    *
+    * @todo Use language files here instead of hardcoded english
+    * @author [A. Gianotto] [<snipe@snipe.net>]
+    * @since  [v1.0]
+    * @return String
+    */
+    public static function relativeTime($ts)
+    {
 
-    if(!ctype_digit($ts))
-			$ts = strtotime($ts);
+        if (!ctype_digit($ts)) {
+            $ts = strtotime($ts);
+        }
 
-		$diff = time() - $ts;
-		if($diff == 0)
-			return 'now';
-		elseif($diff > 0)
-		{
-			$day_diff = floor($diff / 86400);
-			if($day_diff == 0)
-			{
-				if($diff < 60) return 'just now';
-				if($diff < 120) return '1 minute ago';
-				if($diff < 3600) return floor($diff / 60) . ' minutes ago';
-				if($diff < 7200) return '1 hour ago';
-				if($diff < 86400) return floor($diff / 3600) . ' hours ago';
-			}
-			if($day_diff == 1) return 'Yesterday';
-			if($day_diff < 7) return $day_diff . ' days ago';
-			if($day_diff < 31) return ceil($day_diff / 7) . ' weeks ago';
-			if($day_diff < 60) return 'last month';
-			return date('F Y', $ts);
-		}
-		else
-		{
-			$diff = abs($diff);
-			$day_diff = floor($diff / 86400);
-			if($day_diff == 0)
-			{
-				if($diff < 120) return 'in a minute';
-				if($diff < 3600) return 'in ' . floor($diff / 60) . ' minutes';
-				if($diff < 7200) return 'in an hour';
-				if($diff < 86400) return 'in ' . floor($diff / 3600) . ' hours';
-			}
-			if($day_diff == 1) return 'Tomorrow';
-			if($day_diff < 4) return date('l', $ts);
-			if($day_diff < 7 + (7 - date('w'))) return 'next week';
-			if(ceil($day_diff / 7) < 4) return 'in ' . ceil($day_diff / 7) . ' weeks';
-			if(date('n', $ts) == date('n') + 1) return 'next month';
-			return date('F Y', $ts);
-		}
-	}
-
+        $diff = time() - $ts;
+        if ($diff == 0) {
+            return 'now';
+        } elseif ($diff > 0) {
+                $day_diff = floor($diff / 86400);
+            if ($day_diff == 0) {
+                if ($diff < 60) {
+                    return 'just now';
+                }
+                if ($diff < 120) {
+                    return '1 minute ago';
+                }
+                if ($diff < 3600) {
+                    return floor($diff / 60) . ' minutes ago';
+                }
+                if ($diff < 7200) {
+                    return '1 hour ago';
+                }
+                if ($diff < 86400) {
+                    return floor($diff / 3600) . ' hours ago';
+                }
+            }
+            if ($day_diff == 1) {
+                return 'Yesterday';
+            }
+            if ($day_diff < 7) {
+                return $day_diff . ' days ago';
+            }
+            if ($day_diff < 31) {
+                return ceil($day_diff / 7) . ' weeks ago';
+            }
+            if ($day_diff < 60) {
+                return 'last month';
+            }
+            return date('F Y', $ts);
+        } else {
+            $diff = abs($diff);
+            $day_diff = floor($diff / 86400);
+            if ($day_diff == 0) {
+                if ($diff < 120) {
+                    return 'in a minute';
+                }
+                if ($diff < 3600) {
+                    return 'in ' . floor($diff / 60) . ' minutes';
+                }
+                if ($diff < 7200) {
+                    return 'in an hour';
+                }
+                if ($diff < 86400) {
+                    return 'in ' . floor($diff / 3600) . ' hours';
+                }
+            }
+            if ($day_diff == 1) {
+                return 'Tomorrow';
+            }
+            if ($day_diff < 4) {
+                return date('l', $ts);
+            }
+            if ($day_diff < 7 + (7 - date('w'))) {
+                return 'next week';
+            }
+            if (ceil($day_diff / 7) < 4) {
+                return 'in ' . ceil($day_diff / 7) . ' weeks';
+            }
+            if (date('n', $ts) == date('n') + 1) {
+                return 'next month';
+            }
+            return date('F Y', $ts);
+        }
+    }
 }
