dc6ece5de3285fe96cf6221d4d43bf8cc3331933
snipe <snipe@snipe.net>
2016-04-14 17:52:08 -0700

    Attach communities and exchange types to the entry

--- database/seeds/EntrySeeder.php
+++ database/seeds/EntrySeeder.php
@@ -7,6 +7,11 @@ class EntrySeeder extends Seeder
 {
   public function run()
   {
-      factory(Entry::class, 'entry',25)->create();
+      factory(Entry::class, 'entry', 25)->create()->each(function($u) {
+          $u->exchangeTypes()->save(factory(App\Entry::class,'exchange-types')->make());
+          $u->communities()->save(factory(App\Community::class, 'community')->make());
+      });
+
+
   }
 }
