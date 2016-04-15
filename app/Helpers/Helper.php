865a38e8063f67487740d4eab490f94cea9bdb03
snipe <snipe@snipe.net>
2016-04-14 18:14:04 -0700

    Added relativeTime to display times as relative (i.e. "two weeks ago")

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,51 +1,96 @@
 <?php
 
 
-  class Helper
-  {
+class Helper
+{
 
-      public static function cdn_assets($file)
-      {
-          static $manifest = null;
+    public static function cdn_assets($file)
+    {
+        static $manifest = null;
 
-          if (is_null($manifest)) {
+        if (is_null($manifest)) {
             $manifest = json_decode(file_get_contents(public_path('build/rev-manifest.json')), true);
-          }
+        }
 
-          if (isset($manifest['assets/'.$file])) {
-            $cdn_version_url = str_replace('/assets','/',Config::get('services.cdn.default'));
+        if (isset($manifest['assets/'.$file])) {
+            $cdn_version_url = str_replace('/assets', '/', Config::get('services.cdn.default'));
             return $cdn_version_url.$manifest['assets/'.$file];
 
-          } else {
+        } else {
             // This file is not in the versioned manifest, so just include it
             return Config::get('services.cdn.default').'/'.$file;
-          }
+        }
 
         throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
-      }
+    }
 
-      public static function  latlong($location) {
-       		//api call and JSON here
+    public static function latlong($location)
+    {
+          //api call and JSON here
 
-       		if ($location!='') {
-       			try {
-    				$json = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($location));
-    				$parsedjson = json_decode($json, true);
+        if ($location!='') {
+            try {
+                $json = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.urlencode($location));
+                $parsedjson = json_decode($json, true);
 
-    				if (key_exists(0,$parsedjson['results'])) {
-    					$lat_long_array = $parsedjson['results'][0]['geometry']['location'];
-    					return $lat_long_array;
-    				} else {
-    					return false;
-    				}
-    			} catch (Exception $e) {
-        			//echo 'Caught exception: ',  $e->getMessage(), "\n";
-        			return false;
-    			}
-    		}
-    	}
-
-
+                if (key_exists(0, $parsedjson['results'])) {
+                    $lat_long_array = $parsedjson['results'][0]['geometry']['location'];
+                    return $lat_long_array;
+                } else {
+                    return false;
+                }
+            } catch (Exception $e) {
+                //echo 'Caught exception: ',  $e->getMessage(), "\n";
+                return false;
+            }
+        }
+    }
 
 
-  }
+
+    public static function relativeTime($ts) {
+
+    if(!ctype_digit($ts))
+			$ts = strtotime($ts);
+
+		$diff = time() - $ts;
+		if($diff == 0)
+			return 'now';
+		elseif($diff > 0)
+		{
+			$day_diff = floor($diff / 86400);
+			if($day_diff == 0)
+			{
+				if($diff < 60) return 'just now';
+				if($diff < 120) return '1 minute ago';
+				if($diff < 3600) return floor($diff / 60) . ' minutes ago';
+				if($diff < 7200) return '1 hour ago';
+				if($diff < 86400) return floor($diff / 3600) . ' hours ago';
+			}
+			if($day_diff == 1) return 'Yesterday';
+			if($day_diff < 7) return $day_diff . ' days ago';
+			if($day_diff < 31) return ceil($day_diff / 7) . ' weeks ago';
+			if($day_diff < 60) return 'last month';
+			return date('F Y', $ts);
+		}
+		else
+		{
+			$diff = abs($diff);
+			$day_diff = floor($diff / 86400);
+			if($day_diff == 0)
+			{
+				if($diff < 120) return 'in a minute';
+				if($diff < 3600) return 'in ' . floor($diff / 60) . ' minutes';
+				if($diff < 7200) return 'in an hour';
+				if($diff < 86400) return 'in ' . floor($diff / 3600) . ' hours';
+			}
+			if($day_diff == 1) return 'Tomorrow';
+			if($day_diff < 4) return date('l', $ts);
+			if($day_diff < 7 + (7 - date('w'))) return 'next week';
+			if(ceil($day_diff / 7) < 4) return 'in ' . ceil($day_diff / 7) . ' weeks';
+			if(date('n', $ts) == date('n') + 1) return 'next month';
+			return date('F Y', $ts);
+		}
+	}
+
+}
