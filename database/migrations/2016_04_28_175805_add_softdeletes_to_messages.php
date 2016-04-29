f0a39680d0468ecdab87e2ded018fc3f65c5f3fd
snipe <snipe@snipe.net>
2016-04-28 18:08:07 -0700

    Migration for message softdeletes

---
+++ database/migrations/2016_04_28_175805_add_softdeletes_to_messages.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftdeletesToMessages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function ($table) {
+            $table->SoftDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
