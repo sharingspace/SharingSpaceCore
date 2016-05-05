0a82a255462325412911dd4ab1264133fc2e51af
snipe <snipe@snipe.net>
2016-05-04 18:28:14 -0700

    Fixed day in infobox

--- resources/themes/whitelabel/views/home.blade.php
+++ resources/themes/whitelabel/views/home.blade.php
@@ -33,7 +33,7 @@
 
 				<h3 class="uppercase">{{ $whitelabel_group->members->count() }} {{ trans_choice('general.community.members', $whitelabel_group->members->count()) }}</h3>
                 @if ($whitelabel_group->created_at)
-				<p>Since {{ $whitelabel_group->created_at->format('M i, Y') }}</p>
+				<p>Since {{ $whitelabel_group->created_at->format('M j, Y') }}</p>
                 @endif
 			</div>
 
