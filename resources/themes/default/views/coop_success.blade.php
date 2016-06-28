384d7f08ae52acdfdfabc757a4fdf2ba15410169
David Linnard <dslinnard@gmail.com>
2016-06-27 23:52:15 -0700

    Fixes #274 add target="_blank" to social links

--- resources/themes/default/views/coop_success.blade.php
+++ resources/themes/default/views/coop_success.blade.php
@@ -12,11 +12,11 @@
             <p class="lead">In the meantime, let those on social media know that you've taken steps to a fairer and more rewarding economy for all.</p>
             
             <div class="col-md-6">
-                <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//anysha.re/coop">
+                <a href="https://www.facebook.com/sharer/sharer.php?u=https%3A//anysha.re/coop" target="_blank">
                 <img class="coop-share-buttons" src="/assets/img/coop/facebook-share-button.png"></a>
             </div>
             <div class="col-md-6">
-                <a href="https://twitter.com/home?status=I've%20just%20become%20an%20AnyShare%20Coop%20Member%20and%20support%20a%20fairer%20economy%20https%3A//anysha.re/coop%20%23platformcoop%20via%20%40anyshare_coop%20">
+                <a href="https://twitter.com/home?status=I've%20just%20become%20an%20AnyShare%20Coop%20Member%20and%20support%20a%20fairer%20economy%20https%3A//anysha.re/coop%20%23platformcoop%20via%20%40anyshare_coop%20" target="_blank">
                 <img class="coop-share-buttons" src="/assets/img/coop/twitter-share-button.png"></a>
             </div>
         </div>
