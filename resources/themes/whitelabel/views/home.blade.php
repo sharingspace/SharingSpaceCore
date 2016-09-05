b2dbcab7d7263e2c35001f349dee1287d3ce984c
David Linnard <dslinnard@gmail.com>
2016-09-05 11:53:45 -0700

    Fixes #204 moved and renamed exchange column

--- resources/themes/whitelabel/views/home.blade.php
+++ resources/themes/whitelabel/views/home.blade.php
@@ -78,11 +78,11 @@
           <th data-sortable="false" data-field="image">Image</th>
           <th data-sortable="true" data-field="title">{{ trans('general.entries.title') }}</th>
           <th class="hidden-xs" data-sortable="true" data-field="author">{{ trans('general.entries.posted_by') }}</th>
+          <th data-sortable="false" data-field="exchangeTypes">{{ trans('general.entries.exchange') }}</th>
           <th data-sortable="true" data-field="location">{{ trans('general.entries.location') }}</th>
           <th data-sortable="true" data-field="created_at">{{ trans('general.entries.created_at') }}</th>
           <th class="hidden-xs" data-sortable="false" data-field="tags" data-visible="false">{{ trans('general.entries.tags') }}</th>
           <th data-sortable="false" data-field="actions" data-visible="false">{{ trans('general.entries.actions') }}</th>
-          <th data-sortable="false" data-field="exchangeTypes">{{ trans('general.entries.exchange_types') }}</th>
         </tr>
       </thead>
     </table>
