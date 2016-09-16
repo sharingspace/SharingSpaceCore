d01fa4a1aff7430f35c6fbb9bc75bdccab7ec126
snipe <snipe@snipe.net>
2016-09-15 22:18:30 -0700

    Updated transformers to handle pagination

--- app/Http/Transformers/EntriesTransformer.php
+++ app/Http/Transformers/EntriesTransformer.php
@@ -1,32 +1,36 @@
 <?php
 namespace App\Http\Transformers;
 
-use \App\Entry;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class EntriesTransformer
 {
 
-    public function transform(Entry $entry)
+    public function transform(LengthAwarePaginator $entries)
     {
-        return [
-            'id' => $entry->tile_id,
-            'title' => $entry->title,
-            'post_type' => $entry->post_type,
-            'description' => $entry->description,
-            'type' => $entry->post_type,
-            'location' =>
-                [
-                  'name' => $entry->location,
-                  'latitude' => $entry->latitude,
-                  'longitude' => $entry->longitude,
-                ],
-            'author' => $entry->author->fullName(),
-            'created_at' => $entry->created_at,
-            'tags' => $entry->tags,
-            'expires' => $entry->expires,
+        $entries_array = array();
+        foreach ($entries as $entry) {
+
+            $entries_array[] = [
+                'id' => $entry->id,
+                'title' => $entry->title,
+                'post_type' => $entry->post_type,
+                'description' => $entry->description,
+                'location' =>
+                    [
+                        'name' => $entry->location,
+                        'latitude' => $entry->latitude,
+                        'longitude' => $entry->longitude,
+                    ],
+                'author' => $entry->author->getDisplayName(),
+                'created_at' => $entry->created_at,
+                'tags' => $entry->tags,
+                'expires' => $entry->expires,
 
 
 
-        ];
+            ];
+        }
+        return $entries_array;
     }
 }
--- app/Http/Transformers/UserTransformer.php
+++ app/Http/Transformers/UserTransformer.php
@@ -1,32 +1,39 @@
 <?php
 namespace App\Http\Transformers;
 
-use \App\User;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserTransformer
 {
 
-    public function transform(User $user)
+    public function transform(LengthAwarePaginator $users)
     {
-        return [
-            'id' => $user->id,
-            'profile_images' =>
-                [
-                  'avatar' => $user->imagefile,
-                  'cover' => $user->cover_img,
-                ],
-            'first_name' => $user->first_name,
-            'last_name' => $user->last_name,
-            'display_name' => $user->display_name,
-            'bio' => $user->bio,
-            'location' =>
-                [
-                  'name' => $user->location,
-                  'latitude' => $user->latitude,
-                  'longitude' => $user->longitude,
-                ],
-            'created_at' => $user->created_at,
-            'updated_at' => $user->updated_at,
-        ];
+        $users_array = array();
+        foreach ($users as $user) {
+
+            $users_array[] = [
+                'id' => $user->id,
+                'profile_images' =>
+                    [
+                        'avatar' => $user->imagefile,
+                        'cover' => $user->cover_img,
+                    ],
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'display_name' => $user->display_name,
+                'bio' => $user->bio,
+                'location' =>
+                    [
+                        'name' => $user->location,
+                        'latitude' => $user->latitude,
+                        'longitude' => $user->longitude,
+                    ],
+                'created_at' => $user->created_at,
+                'updated_at' => $user->updated_at,
+
+            ];
+        }
+        return $users_array;
+
     }
 }
