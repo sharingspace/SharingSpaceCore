5aa549ff523a1b21723041d7de07087f6bf8b59d
snipe <snipe@snipe.net>
2016-11-17 02:26:21 -0800

    Removed stray end parens on gravatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -198,7 +198,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
         if ($this->gravatar) {
             return "/assets/uploads/users/".$this->id."/".$this->gravatar;
         } else {
-            return "//gravatar.com/avatar/".md5(strtolower(trim($this->email)))."?d=mm' )";
+            return "//gravatar.com/avatar/".md5(strtolower(trim($this->email)))."?d=mm";
         }
     }
 
