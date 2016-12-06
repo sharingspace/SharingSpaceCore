055a0b743510301b21e0a3036ad5353e4b936f99
David Linnard <dslinnard@gmail.com>
2016-12-06 00:11:09 -0800

    Fixes #421 language strings and CTAs

--- resources/themes/default/views/pricing.blade.php
+++ resources/themes/default/views/pricing.blade.php
@@ -56,18 +56,22 @@
   </div>
 </section>
 
-<div class="w-section cta-section">
-<div class="w-container">
-  <div class="w-row">
-    <div class="w-col w-col-9">
-      <h2 class="white-secondary-heading">{{trans('home.cta')}}</h2>
-    </div>
-    <div class="w-col w-col-3">
-      <a href="{{ route('community.create.form') }}" class="w-button cta-button">{{ trans('general.nav.start_now') }}</a>
+<section class="cta">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-8 col-xs-offset-2">
+        <div class="row">
+          <div class="col-sm-9 col-xs-12 margin-bottom-0">
+            <h2 class="white-secondary-heading">{{trans('home.cta')}}</h2>
+          </div>
+          <div class="col-sm-3 col-xs-12 margin-bottom-0">
+            <a href="{{ route('community.create.form') }}" class="btn center-xs pull-right-sm size-18 weight-700 font-smoothing" style="background-color:black;color:white">{{ trans('general.nav.start_now') }}</a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-</div>
+</section>
 <!-- / -->
 
 @stop
