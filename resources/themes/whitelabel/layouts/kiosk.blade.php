4a6ba48d31cc54c79f36f7a948ea4f604316e94e
David Linnard <dslinnard@gmail.com>
2017-01-26 20:00:31 -0800

    Fixes #501 updated corp Google Analystics code

--- resources/themes/whitelabel/layouts/kiosk.blade.php
+++ resources/themes/whitelabel/layouts/kiosk.blade.php
@@ -61,7 +61,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-73360507-1', 'auto');
+  ga('create', 'UA-81177317-1', 'auto');
 
   @if ($whitelabel_group->ga!='')
   ga('create', '{{ $whitelabel_group->ga }}', 'auto', 'clientTracker');
