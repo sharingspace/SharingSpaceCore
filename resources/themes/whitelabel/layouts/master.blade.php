f2424ad0464f77cf381c90b8172dd74074a6be21
David Linnard <dslinnard@gmail.com>
2017-03-11 16:28:33 -0800

    Fixes #5127 google analytics for Shares

--- resources/themes/whitelabel/layouts/master.blade.php
+++ resources/themes/whitelabel/layouts/master.blade.php
@@ -92,18 +92,18 @@
 
 
 @include('partials.geo-lookup')
-
 <script type="text/javascript">
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-81177317-1', 'auto');
 
   @if ($whitelabel_group->ga!='')
-  ga('create', '{{ $whitelabel_group->ga }}', 'auto', 'clientTracker');
+    ga('create', '{{ $whitelabel_group->ga }}', 'auto', 'clientTracker');
+    ga('clientTracker.send', 'pageview');
   @endif
   ga('send', 'pageview');
 
