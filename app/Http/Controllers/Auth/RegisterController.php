7cf0a676b0095ab56b315a5714c5cdd28bfb7766
David Linnard <dslinnard@gmail.com>
2017-03-25 04:24:42 -0700

    Issue 529 (#341)
    * Fixes #532 crash on admin page
    
    * Fixes #531 crash on Share edit
    
    * Fixes #529 email sign-up

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Log;
 
 class RegisterController extends Controller
 {
@@ -48,7 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'display_name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
@@ -63,7 +64,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'display_name' => $data['display_name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
