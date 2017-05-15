cbf874d23e20eef10232624df21db1cc6b92c42f
David Linnard <dslinnard@gmail.com>
2017-05-15 16:37:43 -0700

    Issue 556 (#354)
    * Fixes #539 more hp changes
    
    * Fixes #539 more hp tweaks
    
    * Fixes #556 closed share not seeing list of entries

--- app/Http/Middleware/CommunityPermissionMiddleware.php
+++ app/Http/Middleware/CommunityPermissionMiddleware.php
@@ -28,44 +28,46 @@ class CommunityPermissionMiddleware
     {
         // This is a whitelabel group, otherwise skip it
         if ($request->whitelabel_group) {
-            //LOG::debug('CommunityPermissionMiddleware: This is a valid whitelabel group.');
+            //log::debug('CommunityPermissionMiddleware: This is a valid whitelabel group.');
             if ($request->whitelabel_group->group_type != 'O') {
-               //LOG::debug('CommunityPermissionMiddleware: This is a closed or secret group');
+               //log::debug('CommunityPermissionMiddleware: This is a closed or secret group');
 
                 // If the user is logged in, check that they are a member and are allowed to see this group
                 if (Auth::check()) {
-                    //LOG::debug('CommunityPermissionMiddleware: User is logged in');
+                    //log::debug('CommunityPermissionMiddleware: User is logged in');
                     if (Auth::user()->isMemberOfCommunity($request->whitelabel_group)) {
-                        //LOG::debug('CommunityPermissionMiddleware: This user is authorized to see this community');
+                        //log::debug('CommunityPermissionMiddleware: This user is authorized to see this community');
                         return $next($request);
                     }
                     else if ($request->whitelabel_group->group_type == 'C') {
                         $request_count = $request->whitelabel_group->getRequestCount(Auth::user()->id);
-                        //LOG::debug('CommunityPermissionMiddleware: Closed hub, request_count = '.$request_count);
+                        //log::debug('CommunityPermissionMiddleware: Closed hub, request_count = '.$request_count."  request path=".$request->path()."=");
 
-                        if ($request->path() == "members") {
+                        if (($request->path() == "members") || ($request->path() == "entry/json.browse") || ($request->path() == "/")) {
+                            //log::debug('CommunityPermissionMiddleware: we let users see the browse and members page');
+
                             // they can see this page
                             return $next($request);
                         }
                         else if ($request_count) {
                             $request_count++; // user alreday has a request pending, this will trigger the correct message
-                            //LOG::debug('CommunityPermissionMiddleware: user has already requested to join Share '.$request_count.'   '.$request->whitelabel_group->name);
+                            //log::debug('CommunityPermissionMiddleware: user has already requested to join Share '.$request_count.'   '.$request->whitelabel_group->name);
                             return redirect()->route('community.request-access.form');
                         }
                         else {
-                            //LOG::debug('CommunityPermissionMiddleware: first request to join Share '.$request->whitelabel_group->name);
+                            //log::debug('CommunityPermissionMiddleware: first request to join Share '.$request->whitelabel_group->name);
                             return redirect()->route('community.request-access.form');
                         }
                     }
                     else {
-                        //LOG::debug('CommunityPermissionMiddleware: Hub is secret but user is not a member');
+                        //log::debug('CommunityPermissionMiddleware: Hub is secret but user is not a member');
                         return view('auth/login-unbranded')->withError('Secret Hub. You must be invited to join this hub');
                     }
                 }
                 else { // User is not logged in
-                    LOG::debug('CommunityPermissionMiddleware: User is not logged in.');
+                    //log::debug('CommunityPermissionMiddleware: User is not logged in.');
                     if ($request->whitelabel_group->group_type == 'S') {
-                        //LOG::debug('CommunityPermissionMiddleware: User is not logged in and this community is secret.');
+                        //log::debug('CommunityPermissionMiddleware: User is not logged in and this community is secret.');
                         return redirect()->route('login.unbranded')->withError('This hub is secret. You must be invited to join this hub.');
                     }
                     else {
@@ -73,22 +75,22 @@ class CommunityPermissionMiddleware
                             // we let people see some pages of an open share
                             return $next($request);
                         }
-                        //LOG::debug('CommunityPermissionMiddleware: User is not logged in and this community is closed, redirecting to login page');
+                        //log::debug('CommunityPermissionMiddleware: User is not logged in and this community is closed, redirecting to login page');
                         return redirect()->route('login');
                     }
                 }
             }
             else { // This group is not restricted, ie open
-                //LOG::debug('CommunityPermissionMiddleware: open group.');
+                //log::debug('CommunityPermissionMiddleware: open group.');
 
                 return $next($request);
                 if (Auth::check()) {
                     if (Auth::user()->isMemberOfCommunity($request->whitelabel_group)) {
-                        //LOG::debug('CommunityPermissionMiddleware: This user is a member of this open Share');
+                        //log::debug('CommunityPermissionMiddleware: This user is a member of this open Share');
                         return $next($request);
                     }
                     else {
-                        //LOG::debug('CommunityPermissionMiddleware: Open hub but user is not a member, path = ('.$request->path().') ('."users/".Auth::user()->id.")");
+                        //log::debug('CommunityPermissionMiddleware: Open hub but user is not a member, path = ('.$request->path().') ('."users/".Auth::user()->id.")");
 
                         if ($request->path() == "/" || $request->path() == "entry/json.browse" || $request->path() == "users/".Auth::user()->id ) {
                             // we let people see some pages of an open share
@@ -100,7 +102,7 @@ class CommunityPermissionMiddleware
                     }
                 }
                 else {
-                    //LOG::debug('CommunityPermissionMiddleware: open hub and user not logged in');
+                    //log::debug('CommunityPermissionMiddleware: open hub and user not logged in');
                 }
 
             }
