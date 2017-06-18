eb0641d33f4d36214caec84d276e43ed11b5542d
David Linnard <dslinnard@gmail.com>
2017-06-18 19:13:58 -0400

    Fixes session messages not working
    Looks like kernel.php was including StartSession twice

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,8 +30,6 @@ class Kernel extends HttpKernel
         'web' => [ // THIS IS THE GROUP WE ARE TALKING ABOUT
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
         ],
         'api' => [
