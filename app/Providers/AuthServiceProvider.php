5cb90f6c6d81357a97530ec0d58362822be99f20
David Linnard <dslinnard@gmail.com>
2017-06-29 19:30:01 -0400

    Fixes #564 gates now working after I broke them. Oh boy.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -37,14 +37,21 @@ class AuthServiceProvider extends ServiceProvider
     // that shouldn't be true, check that the user in question
     // isn't a superadmin
     // --------------------------------
+
+
+    // NOTE. gates are only called when user is logged in
     // If the user is a super admin, let them through no matter what
     Gate::before(function ($user, $ability) {
-        return ($user->superadmin == '1');
+        if ($user->superadmin == '1') {
+            return true;
+        }
     });
 
     // Check if the user can see the community entries
     Gate::define('admin', function ($user) {
-        return ($user->superadmin == '1');
+        if ($user->superadmin == '1') {
+            return true;
+        }    
     });
 
 
@@ -55,7 +62,9 @@ class AuthServiceProvider extends ServiceProvider
     // Check if the user can see the community entries
     Gate::define('view-browse', function ($user, $community) {
         //Log::debug("view-browse gate");
-        return ($user->canSeeCommunity($community) || $community->group_type != 'S');
+        if ($user->canSeeCommunity($community) || $community->group_type != 'S') {
+            return true;
+        }
     });
 
     // Check if the user can see the community about page
@@ -72,28 +81,32 @@ class AuthServiceProvider extends ServiceProvider
                 return true;
             }
         }
-        
-        return false;
     });
 
     // Check if the user can see the community members page
     Gate::define('view-members', function ($user, $community) {
         //Log::debug("view-members gate");
-        return ($community->group_type != 'S');
+        if ($community->group_type != 'S') {
+            return true;
+        }
     });
 
     // Check if the user can join a community
     // (they are not already a member)
     Gate::define('join-community', function ($user, $community) {
         //Log::debug("join-community gate");
-        return !$user->isMemberOfCommunity($community);
+        if (!$user->isMemberOfCommunity($community)) {
+            return true;
+        }
     });
 
     // Check if the user can update the community settings
     // (they are an admin)
     Gate::define('update-community', function ($user, $community) {
         //Log::debug("update-community gate");
-        return $user->isAdminOfCommunity($community);
+        if ($user->isAdminOfCommunity($community)) {
+            return true;
+        }
     });
 
 
@@ -105,21 +118,27 @@ class AuthServiceProvider extends ServiceProvider
     // Check the user is a member of the community and cam therefore post
     // an entry
     Gate::define('post-entry', function ($user, $community) {
-      //Log::debug("post-entry gate");
-      return $user->isMemberOfCommunity($community);
+        //Log::debug("post-entry gate");
+        if ($user->isMemberOfCommunity($community)) {
+            return true;
+        }
     });
 
     // Check that the user can view an entry
     Gate::define('view-entry', function ($user, $community) {
        //Log::debug("view-entry gate");
-        return (($community->group_type == 'O') || ($user->isMemberOfCommunity($community)));
+        if (($community->group_type == 'O') || ($user->isMemberOfCommunity($community))) {
+            return true;
+        }
     });
 
 
     // Check if the user can update an entry
     Gate::define('update-entry', function ($user, $entry) {
         //Log::debug("update-entry gate");
-        return ($user->id === $entry->created_by);
+        if ($user->id === $entry->created_by) {
+            return true;
+        }
     });
 
     // --------------------------------
@@ -128,7 +147,9 @@ class AuthServiceProvider extends ServiceProvider
 
     // Check the user is part of the conversation and can therefore view it
     Gate::define('view-conversation', function ($user, $conversation) {
-        return $conversation->checkUserInConvo($user);
+        if ($conversation->checkUserInConvo($user)) {
+            return true;
+        }
     });
 
     // --------------------------------
@@ -138,19 +159,25 @@ class AuthServiceProvider extends ServiceProvider
     // Check whether the user can edit a users profile
     Gate::define('update-profile', function ($user, $profile_id) {
        //log::debug("update-profile gate: ".$user->id ."   ". $profile_id);
-        return ($user->id === $profile_id);
+        if ($user->id === $profile_id) {
+            return true;
+        }
     });
 
     // Check whether the user can make an offer on an entry
     Gate::define('make-offer', function ($user, $entry, $community) {
        //log::debug("make-offer gate: ".$user->id ."   ". $entry->id ."   ". $community->name);
-        return (($entry->created_by != $user->id) && $user->isMemberOfCommunity($community) && (!$entry->expired) && ($entry->completed_at == ''));
+        if (($entry->created_by != $user->id) && $user->isMemberOfCommunity($community) && (!$entry->expired) && ($entry->completed_at == '')) {
+            return true;
+        }
     });
 
     // Check whether the user can send a message to another user
     Gate::define('send-msg', function ($user, $profile_id, $community) {
         //log::debug("send-msg gate user profile id ".$profile_id ."   logged in user id ".$user->id);
-        return ($profile_id != $user->id) && $user->isMemberOfCommunity($community);
+        if (($profile_id != $user->id) && $user->isMemberOfCommunity($community)) {
+            return true;
+        }
     });
   }
 }
