e142572140b6a0a7b4f48ea93daae0aeeb5e4701
Vanderlei Sbaraini Amancio <harrysbaraini@gmail.com>
2017-12-15 01:00:40 -0200

    map

---
+++ resources/themes/whitelabel/partials/map.blade.php
@@ -0,0 +1,10 @@
+<div id="map-area">
+    <div id="map-area-buttons">
+    <button type="button" class="map-area__topFloor">Top Floor</button>
+    <button type="button" class="map-area__moveUp">Move Up</button>
+    <button type="button" class="map-area__moveDown">Move Down</button>
+    <button type="button" class="map-area__bottomFloor">Bottom Floor</button>
+    <button type="button" class="map-area__exitIndoors">Exit</button>
+    </div>
+    <div id="entry_browse_map" style="height: 320px;"></div>
+</div>
