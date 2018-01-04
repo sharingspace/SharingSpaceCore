a7b18677b4b8fb1707da7713e36db9272448baa0
Vanderlei Sbaraini Amancio <harrysbaraini@gmail.com>
2018-01-04 10:49:50 -0200

    show maps even when there are no locations

--- resources/themes/whitelabel/partials/map.blade.php
+++ resources/themes/whitelabel/partials/map.blade.php
@@ -1,6 +1,4 @@
-@if ($whitelabel_group->latitude && $whitelabel_group->longitude)
-    <div id="map-area" style="position: relative; margin-top: 4px; margin-bottom: 4px;">
-        <div id="entry_browse_map_widget" class="wrld-widget-container"></div>
-        <div id="entry_browse_map" style="height: {{ isset($size) ? $size : '460' }}px;"></div>
-    </div>
-@endif
+<div id="map-area" style="position: relative; margin-top: 4px; margin-bottom: 4px;">
+    <div id="entry_browse_map_widget" class="wrld-widget-container"></div>
+    <div id="entry_browse_map" style="height: {{ isset($size) ? $size : '460' }}px;"></div>
+</div>
