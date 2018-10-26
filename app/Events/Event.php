7639dabfee310cdd46c112af007d01dbdd464772
Dhaval <dhaval48@gmail.com>
2018-10-26 15:23:54 +0530

    working on github webhook

--- app/Events/Event.php
+++ app/Events/Event.php
@@ -7,6 +7,6 @@ abstract class Event
 
     Event::listen('github.webhook', function()
 	{
-	  shell_exec('cd /sites/anyshare &&  /usr/bin/git checkout master-update 2>&1');	 
+	  shell_exec('cd ~/sites/anyshare &&  /usr/bin/git checkout master-update 2>&1');	 
 	});
 }
